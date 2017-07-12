f11b78e575ecc98ed5800d2010f15e720cab6dcf
Samuel Deng <samuel.deng@istuary.com>
2017-07-12 15:40:21 -0400

    update

--- Python/try.py
+++ Python/try.py
@@ -1,25 +1,27 @@
 #!/usr/bin/ python3.5
 # -*- coding: utf-8 -*-
-from random import *
-from functools import reduce
-from collections import OrderedDict
-import pickle
-from time import time
-from itertools import islice
-from itertools import *
-from pprint import pprint
-
-class IntTuple(tuple):
-    def __new__(cls, iterable):
-        g = (x for x in iterable if isinstance(x,int) and x>0)
-        return super(IntTuple, cls).__new__(cls,g)
-
-
-    def __init__(self, f):
-        super(IntTuple, self).__init__()
-
-t = IntTuple([12,-2,'wer'])
-
-print(t)
+# from random import *
+# from functools import reduce
+# from collections import OrderedDict
+# import pickle
+# from time import time
+# from itertools import islice
+# from itertools import *
+# from pprint import pprint
+#
+# class IntTuple(tuple):
+#     def __new__(cls, iterable):
+#         g = (x for x in iterable if isinstance(x,int) and x>0)
+#         return super(IntTuple, cls).__new__(cls,g)
+#
+#
+#     def __init__(self, f):
+#         super(IntTuple, self).__init__()
+#
+# t = IntTuple([12,-2,'wer'])
+#
+# print(t)
+import os
+print(os.getcwd())
 
 
